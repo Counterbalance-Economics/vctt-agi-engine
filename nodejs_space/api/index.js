9e7f7c5fe1e268910a2fec3d941e0bbc4a9d5b48
DeepAgent <deepagent@abacus.ai>
2025-11-17 02:21:50 +0000

    f2c5881b-6c45-4589-a86c-62089294a177

---
+++ nodejs_space/api/index.js
@@ -0,0 +1,17 @@
+
+// Vercel serverless entry point
+const { NestFactory } = require('@nestjs/core');
+const { AppModule } = require('../dist/app.module');
+
+let app;
+
+module.exports = async (req, res) => {
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
+  }
+
+  const server = app.getHttpAdapter().getInstance();
+  return server(req, res);
+};
